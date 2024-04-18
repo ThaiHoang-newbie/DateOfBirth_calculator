fbeb9c25f879908a4b9d1e0ce3fb20cf83568ec6
Thai Hoang <thaihoang20112k3@gmail.com>
2024-04-18 11:55:52 +0700

    feat/search

--- src/main/java/com/example/demo/DateOfBirthCalculatorApplication.java
+++ src/main/java/com/example/demo/DateOfBirthCalculatorApplication.java
@@ -1,22 +1,26 @@
 package com.example.demo;
-import org.springframework.boot.SpringApplication;
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import controllers.JSONSearch;
 import validations.InputValidator;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class DateOfBirthCalculatorApplication {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String personName = scanner.nextLine();
         System.out.print("Enter the date (dd-mm-yyyy): ");
         String dateString = scanner.next();
+        scanner.nextLine();
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate date = LocalDate.parse(dateString, formatter);
@@ -25,15 +29,35 @@ public class DateOfBirthCalculatorApplication {
         int year = date.getYear();
 
         Person person = new Person(personName, java.sql.Date.valueOf(date));
+        String filePath = "data.json";
 
-        if (!InputValidator.isValidDay(day, month, year) ) {
+        if (!InputValidator.isValidDay(day, month, year)) {
             System.out.println("Invalid date of birth, please check again");
         } else {
             person.saveUserInfo();
             person.saveUserInfoToFile("data.json");
-            System.out.println(person.getName() + " has " + person.calculateAge() + " age, " + person.calculateElement() + " element, "
+            System.out.println(person.getName() + " has " + person.calculateAge() + " age, " + person.calculateElement()
+                    + " element, "
                     + "and " + person.ZodiacCalculator() + " zodiac ");
         }
-	}
+
+        // search usage
+        System.out.print("Enter your name that you wanna search: ");
+        String nameSearch = scanner.nextLine();
+
+        try {
+            JSONSearch jsonSearch = new JSONSearch(filePath);
+            List<String> searchResults = jsonSearch.searchByName(nameSearch);
+            if (searchResults.size() != 0) {
+                for (String result : searchResults) {
+                    System.out.println(result);
+                }
+            } else {
+                System.out.println("Data not found");
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
